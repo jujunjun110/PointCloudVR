2ceb93b174548a64420ec2e2a90ea6b342acc0d9
Jun Ito <jun_ito@voyagegroup.com>
2020-05-24 00:34:01 +0900

    Update: refactoring

--- Assets/Scipts/PtsReader.cs
+++ Assets/Scipts/PtsReader.cs
@@ -4,25 +4,21 @@ using UnityEngine;
 using System.IO;
 using System.Linq;
 public static class PtsReader {
-    public static List<(Vector3, Vector3)> Load(TextAsset csv) {
-        var rows = csv.text.Split('\n');
+    public static List<(Vector3, Vector3)> Load(TextAsset ptsfile) {
+        var rows = ptsfile.text.Split('\n');
         return rows.Where(s => s != "").Select(row => parseRow(row)).ToList();
-    }
-    public static List<(Vector3, Vector3)> LoadWithPath(string path = null) {
-        var csv = Resources.Load(path, typeof(TextAsset)) as TextAsset;
-        return Load(csv);
     }
 
     private static (Vector3, Vector3) parseRow(string row) {
-        var parsed = row.Split(' ');
+        var splitted = row.Split(' ');
         return (new Vector3(
-            float.Parse(parsed[0]),
-            float.Parse(parsed[2]), // Z-up -> Y-up
-            float.Parse(parsed[1])  // Z-up -> Y-up
+            float.Parse(splitted[0]),
+            float.Parse(splitted[2]), // Z-up -> Y-up
+            float.Parse(splitted[1])  // Z-up -> Y-up
         ), new Vector3(
-            float.Parse(parsed[3]),
-            float.Parse(parsed[4]),
-            float.Parse(parsed[5])
+            float.Parse(splitted[3]),
+            float.Parse(splitted[4]),
+            float.Parse(splitted[5])
         ));
     }
 }
